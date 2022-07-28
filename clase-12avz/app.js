2e4763216a3a9a3874f6496dffaeb054cd6f0d0d
gojaguerra <jguerra1968@gmail.com>
2022-07-28 15:36:46 -0300

    operadores avanzados/librerias bootstrap/disabled botones

---
+++ clase-12avz/app.js
@@ -0,0 +1,58 @@
+import {
+    productos
+} from './stock.js'
+import {
+    carritoIndex
+} from './carritoIndex.js';
+
+const mostrarProductos = (productos) => {
+    const contenedorProductos = document.getElementById('producto-contenedor');
+
+    productos.forEach(producto => {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML += `<div class="card" style="width: 18rem;">
+                            <img src="${producto.img}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text">Descripción:  ${producto.desc}</p>
+                                <p class="card-text">Precio:$ ${producto.precio}</p>
+                                <button class="btn btn-primary" id=boton${producto.id}>Comprar</button>
+                            </div>
+                        </div>`
+
+        contenedorProductos.appendChild(div)
+
+        const boton = document.getElementById(`boton${producto.id}`)
+        boton.addEventListener('click', () => {
+            carritoIndex(producto.id);
+            // alert(`Se agrego ${producto.nombre}`);
+
+        })
+
+    })
+
+}
+
+export function verificoStorage() {
+    
+    // uso de OPERADORES AVANZADOS LOGICOS OR
+    const listaProductos = JSON.parse(localStorage.getItem("carritoFG")) || [];
+    
+    // pongo el contador en cero, por si no hay items en el carrito
+    let cantidadPedido = document.getElementById("contador-carrito");
+    let cantidad=0;
+    cantidadPedido.innerHTML = `${cantidad}`;
+
+    listaProductos.forEach(ele => {
+        let i=0;
+        for(i=0;i<ele.cantidad;i++){
+        carritoIndex(ele.id);
+        }
+    })
+
+}
+
+mostrarProductos(productos);
+
+verificoStorage();
---
+++ clase-12avz/modal.js
@@ -0,0 +1,107 @@
+import { verificoStorage } from "./app.js";
+import { carritoVaciar } from "./carritoIndex.js";
+
+/* const modalContenedor = document.querySelector('.modal-container');
+const modalCarrito = document.querySelector('.modal-carrito')
+const abrirCarrito = document.getElementById('open')
+ */
+
+const cerrarCarrito = document.getElementById('cerrar')
+const finalizarCarrito = document.getElementById('finalizar')
+const enviarCarrito = document.getElementById('enviar')
+
+// CLICK PARA ABRIR EL CARRITO - EN EL HEADER
+/* abrirCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+}) */
+
+/* // CLICK BOTON CERRAR EN EL CARRITO - EN EL MODAL
+cerrarCarrito.addEventListener('click', () => {
+    modalContenedor.classList.remove('modal-active')
+})
+
+// CLICK FUERA DEL MODAL PARA CERRAR MODAL
+modalContenedor.addEventListener('click', () => {
+    cerrarCarrito.click();
+}) */
+
+/* modalCarrito.addEventListener('click', (e) => {
+    e.stopPropagation();
+}) */
+
+/* cerrarCarrito.addEventListener('click', () => {
+    modalContenedor.classList.remove('modal-active')
+}) */
+
+// muestro el pedido antes de mandarlo
+finalizarCarrito.addEventListener('click', () => {
+
+    let contenedor = document.getElementById("modalFinal");
+    let productos = JSON.parse(localStorage.getItem("carritoFG"));
+    let cantidad;
+
+    // uso de OPERADORES AVANZADOS TERNARIOS y VERIFICO CANTIDAD DE ITEMS>0
+    productos ? cantidad = productos.reduce((acumulador, precio) => acumulador + (precio.cantidad), 0) : 0;
+
+    if(productos && cantidad>0){
+        
+        // PONGO EL BOTON COMO PRIMARIO y ENABLED
+        enviarCarrito.classList.remove('btn-secondary')
+        enviarCarrito.classList.add('btn-primary')
+        enviarCarrito.removeAttribute("disabled")
+
+        contenedor.innerHTML = "<h2>Confirmación de pedido</h2><br>";
+        productos.forEach(element => {
+
+            if(element.cantidad>0){
+                let item = document.createElement("div");
+                item.innerHTML = `cantidad: ${element.cantidad}
+                                ${element.nombre}
+                                - precio unit: $${element.precio}`;
+                contenedor.append(item);
+            }
+        });
+
+        const precioPedido = productos.reduce((acumulador, precio) => acumulador + (precio.precio * precio.cantidad), 0)
+        
+        let item = document.createElement("div");
+        item.innerHTML = `<br><h3> TOTAL: $${precioPedido}</h3>`
+        contenedor.append(item);
+    
+    } else {
+        
+        contenedor.innerHTML = "<h2>No habia ningún pedido!</h2><br>";
+
+        // PONGO EL BOTON COMO SECUNDARIO y DISABLED
+        enviarCarrito.classList.remove('btn-primary')
+        enviarCarrito.classList.add('btn-secondary')
+        enviarCarrito.setAttribute("disabled","")
+    }
+ 
+})
+
+
+// ENVIO pedido y vacio local storege
+enviar.addEventListener('click', () => {
+
+    // borra el storage
+    localStorage.clear();
+    // borra el array de pedidos
+    carritoVaciar();
+    // cierra formulario modal
+    cerrarCarrito.click();
+    // acomoda contadores del carrito header
+    verificoStorage();
+
+    // limpia modal del pedido
+    const contenedorCarrito = document.getElementById("carrito-contenedor")
+
+    if(contenedorCarrito){
+        contenedorCarrito.innerHTML = "";
+    }
+
+    let totalPedido = document.getElementById("elTotal");
+    let precioPedido=0;
+    totalPedido.innerHTML = `Total: $${precioPedido}`;
+
+})
